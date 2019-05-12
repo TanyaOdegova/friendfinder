1b32f7ce46f6a3b512c642d05b4484c2d1c25e0d
TanyaOdegova <odegovat@yahoo.com>
2019-05-11 20:38:36 -0400

    <added friends images updated survey css installed body-parser>

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,34 @@
 var friends = [
     {
         name: "Ross",
-        photo: "",
+        photo: "../app/public/assets/images/ross.jpg",
         scores: [5,1,2,3,1,2,5,1,1,1]
     },
     {
         name: "Monica",
-        photo: "",
+        photo: "../app/public/asssets/images/monica.jpg",
         scores: [2,5,4,5,1,2,4,3,2,3]
     },
     {
         name: "Chandler",
-        photo: "",
+        photo: "../app/public/asssets/images/chandler.jpg",
         scores: [1,3,4,4,5,3,2,1,2,2]
     },
     {
         name: "Joey",
-        photo: "",
+        photo: "../app/public/asssets/images/joe.jpg",
         scores: [1,2,1,1,1,1,1,1,1,1]
     },
     {
         name: "Rachel",
-        photo: "",
+        photo: "../app/public/asssets/images/rachel.jpg",
         scores: [5,5,5,5,5,5,5,5,5,5]
     },
+    {
+        name: "Phoebe",
+        photo: "../app/public/asssets/images/phoebe.jpg",
+        scores: [5,5,5,5,1,5,1,1,5,5]
+    }
 ];
 
 module.exports = friends;
