711992b62d0d18d6a8a79af26a437006cef2cc3d
TanyaOdegova <odegovat@yahoo.com>
2019-05-11 20:04:03 -0400

    <prelim commit>

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        name: "Ross",
+        photo: "",
+        scores: [5,1,2,3,1,2,5,1,1,1]
+    },
+    {
+        name: "Monica",
+        photo: "",
+        scores: [2,5,4,5,1,2,4,3,2,3]
+    },
+    {
+        name: "Chandler",
+        photo: "",
+        scores: [1,3,4,4,5,3,2,1,2,2]
+    },
+    {
+        name: "Joey",
+        photo: "",
+        scores: [1,2,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Rachel",
+        photo: "",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+];
+
+module.exports = friends;
