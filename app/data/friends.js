086626438da55b40abdf0e4c908632d901661bed
TanyaOdegova <odegovat@yahoo.com>
2019-05-11 21:27:47 -0400

    tanya o friend finder app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,5 @@
+/* Homework requirement: application's data inside of app/data/friends.js as an array of objects. 
+Each of these objects should roughly follow the format below.*/
 var friends = [
     {
         name: "Ross",
