af528783fa7c79d176cb24411e865cea4b018b1a
TanyaOdegova <odegovat@yahoo.com>
2019-05-11 22:46:19 -0400

    css fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-/* Homework requirement: application's data inside of app/data/friends.js as an array of objects. 
+/* Homework requirement:application's data inside of app/data/friends.js as an array of objects. 
 Each of these objects should roughly follow the format below.*/
 var friends = [
     {
