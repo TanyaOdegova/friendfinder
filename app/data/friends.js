6708eff9c72ea9d62b71f1a136f65a0fdb5c432d
TanyaOdegova <odegovat@yahoo.com>
2019-05-27 18:35:56 -0400

    fixed questions data file

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,32 +3,32 @@ var friends = [
     {
         name: "Ross",
         photo: "static/assets/images/ross.jpg",
-        scores: [5,1,2,3,1,2,5,1,1,1]
+        scores: [5,1,1,1,1,1,2,1,4,5]
     },
     {
         name: "Monica",
         photo: "static/assets/images/monica.jpg",
-        scores: [2,5,4,5,1,2,4,3,2,3]
+        scores: [2,4,5,1,1,5,5,5,2,5]
     },
     {
         name: "Chandler",
         photo: "static/assets/images/chandler.jpg",
-        scores: [1,3,4,4,5,3,2,1,2,2]
+        scores: [2,5,1,3,5,1,5,1,5,4]
     },
     {
         name: "Joey",
         photo: "static/assets/images/joe.jpg",
-        scores: [1,2,1,1,1,1,1,1,1,1]
+        scores: [1,5,1,1,5,5,1,1,1,1]
     },
     {
         name: "Rachel",
         photo: "staticassets/images/rachel.jpg",
-        scores: [5,5,5,5,5,5,5,5,5,5]
+        scores: [4,5,2,1,5,5,1,5,1,1]
     },
     {
         name: "Phoebe",
         photo: "static/assets/images/phoebe.jpg",
-        scores: [5,5,5,5,1,5,1,1,5,5]
+        scores: [1,5,1,5,1,5,1,3,1,1]
     }
 ];
 // Module.Exports will export the array. 
